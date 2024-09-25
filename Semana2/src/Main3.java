e67e3bdb9216805e4579e4efbe585209273943c6
fx0n <hugomrserra@gmail.com>
2024-09-25 22:04:06 +0100

    Updated 25/09 22:04

--- Semana2/src/Main3.java
+++ Semana2/src/Main3.java
@@ -4,7 +4,7 @@ public class Main3 {
     public static void main(String[] args){
         ArrayList<Aluno> alunos = Aluno.lerAlunos("Semana2/Estudantes.txt");
 
-        Disciplina disciplina = Disciplina.criarDisciplina("Programação", 3, alunos);
+        Disciplina disciplina = Disciplina.criarDisciplina("Programming", 3, alunos);
 
         System.out.println(disciplina);
     }
