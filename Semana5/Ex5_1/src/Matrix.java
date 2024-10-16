0186e0a37752443e5a5c7897a6774f7b11d36a5f
fx0n <hugomrserra@gmail.com>
2024-10-16 11:19:36 +0100

    Initial Commit

---
+++ Semana5/Ex5_1/src/Matrix.java
@@ -0,0 +1,14 @@
+public class Matrix {
+
+    int lines;
+    int columns;
+    int[][] matrix;
+
+    public Matrix(int lines, int columns) {
+        this.lines = lines;
+        this.columns = columns;
+        this.matrix = new int[lines][columns];
+    }
+
+    
+}
